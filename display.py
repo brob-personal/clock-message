c24cb6c732e2a11a06c202a05f9cfb35300b104f
Brian Robinson <brianjrobinson03@gmail.com>
2026-09-02 21:32:22 -0400

    Initial Hello World & Display.py

---
+++ display.py
@@ -0,0 +1,21 @@
+import time
+from picographics import PicoGraphics, DISPLAY_SCROLL_PACK, PEN_P8
+from picoscroll import PicoScroll
+
+def scroll_message(text, speed=0.1):
+    graphics = PicoGraphics(DISPLAY_SCROLL_PACK, pen_type=PEN_P8)
+    scroll = PicoScroll()
+    t = scroll.get_width()
+    wrap = -graphics.measure_text(text, scale=0)
+
+    while t > wrap:
+        graphics.set_pen(0)
+        graphics.clear()
+        graphics.set_pen(255)
+        graphics.text(text, t, 0, scale=1)
+        scroll.update(graphics)
+        t -= 1
+        time.sleep(speed)
+
+if __name__ == "__main__":
+    scroll_message("Hello World")
